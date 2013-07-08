6647698cc15cd5c6670bc9d2192a848101674599
matrix <matrix@sat-desktop>
2013-07-08 18:16:26 +0530

    created my printer

---
+++ SatGitProMain/src/satgitpromain/MyPrinter.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package satgitpromain;
+
+/**
+ *
+ * @author matrix
+ */
+public class MyPrinter {
+    
+    public static final String PRO_NAME="GIT_TEST";
+    
+    public static void printInfo(){
+        System.out.println("ProName:"+PRO_NAME);
+    }
+    
+}
